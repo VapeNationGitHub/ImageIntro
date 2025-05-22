7506031ec9216c4c72212ff6a4adedd1a1a1d71b
Crocko47 <teoremaabela@yahoo.com>
2025-05-22 12:51:19 +0800

    Спринт 9. Тема 1. Онюординг

---
+++ ImageIntro/Profile/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet private var profileImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var messageLabel: UILabel!
+    @IBOutlet private var exitButton: UIButton!
+    
+    @IBAction private func didTapExitButton() {
+        
+    }
+}
