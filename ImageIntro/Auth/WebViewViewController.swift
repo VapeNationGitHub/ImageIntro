59eaf43a7ef1848cf892f852c48892c3bd3f9894
Crocko47 <teoremaabela@yahoo.com>
2025-05-30 22:22:30 +0800

    Добавлен URLComponents

--- ImageIntro/Auth/WebViewViewController.swift
+++ ImageIntro/Auth/WebViewViewController.swift
@@ -1,10 +1,41 @@
 import WebKit
 import UIKit
 
+enum WebViewConstants {
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
 final class WebViewViewController: UIViewController {
     
     @IBOutlet private var webView: WKWebView!
     @IBAction func didTapBackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    // MARK: - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadAuthView()
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            return
+        }
+
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+
+        guard let url = urlComponents.url else {
+            return
+        }
+
+        let request = URLRequest(url: url)
+        webView.load(request)
+        }
 }
--- ImageIntro/Constants.swift
+++ ImageIntro/Constants.swift
@@ -3,7 +3,7 @@ import Foundation
 enum Constants {
     static let accessKey = "uPB0L8MgssoSQer-oVMyykFJI7_G-JE8LrO19QY1O4U"
     static let secretKey = "ti-b7fBml78hZFJMf1QCGDEiQe5ftDB7cHY0sSd6pf0"
-    static let redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     
     static let accessScope = "public+read_user+write_likes"
     
