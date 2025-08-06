d5a0ebe5eb092fb261370a5ea06e0deef125209f
Crocko47 <teoremaabela@yahoo.com>
2025-08-06 15:35:35 +0800

    Восстановление работы приложения, не отображается фотография профиля и логин.

--- ImageIntro/TabBarController.swift
+++ ImageIntro/TabBarController.swift
@@ -3,14 +3,20 @@ import UIKit
 final class TabBarController: UITabBarController {
     override func awakeFromNib() {
         super.awakeFromNib()
+
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
         let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
         let profileViewController = ProfileViewController()
+
+        let presenter = ProfilePresenter()
+        profileViewController.configure(presenter)
+
         profileViewController.tabBarItem = UITabBarItem(
-            title: "", // если подпись не нужна, оставьте пустую строку
+            title: "",
             image: UIImage(named: "tab_profile_active"),
             selectedImage: nil
         )
         self.viewControllers = [imagesListViewController, profileViewController]
     }
 }
+
