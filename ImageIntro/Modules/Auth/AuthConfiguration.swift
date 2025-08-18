ce09e2996d7e9725865b0071f07f123071f3350e
Crocko47 <teoremaabela@yahoo.com>
2025-08-18 22:00:14 +0800

    refactor: OAuth2Service.swift, AuthConfiguration.swift

--- ImageIntro/Modules/Auth/AuthConfiguration.swift
+++ ImageIntro/Modules/Auth/AuthConfiguration.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 enum Constants {
-    static let AccessKey = "uPB0L8MgssoSQer-oVMyykFJI7_G-JE8LrO19QY1O4U"
-    static let SecretKey = "ti-b7fBml78hZFJMf1QCGDEiQe5ftDB7cHY0sSd6pf0"
-    static let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let AccessScope = "public+read_user+write_likes"
-    static let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let accessKey = "uPB0L8MgssoSQer-oVMyykFJI7_G-JE8LrO19QY1O4U"
+    static let secretKey = "ti-b7fBml78hZFJMf1QCGDEiQe5ftDB7cHY0sSd6pf0"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
     static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 }
 
@@ -22,12 +22,12 @@ struct AuthConfiguration {
     // MARK: - Стандартная конфигурация (прод)
     static var standard: AuthConfiguration {
         return AuthConfiguration(
-            accessKey: Constants.AccessKey,
-            secretKey: Constants.SecretKey,
-            redirectURI: Constants.RedirectURI,
-            accessScope: Constants.AccessScope,
+            accessKey: Constants.accessKey,
+            secretKey: Constants.secretKey,
+            redirectURI: Constants.redirectURI,
+            accessScope: Constants.accessScope,
             authURLString: Constants.unsplashAuthorizeURLString,
-            defaultBaseURL: Constants.DefaultBaseURL
+            defaultBaseURL: Constants.defaultBaseURL
         )
     }
 }
